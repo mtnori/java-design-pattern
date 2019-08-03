2707d154b498c679032c50df75430fb1d89d2197
mtnori <moto44.noriyuki@gmail.com>
2019-08-03 09:59:56 +0900

    Abstract Factoryパターンを追加

---
+++ src/abstractfactory/Main.java
@@ -0,0 +1,40 @@
+package abstractfactory;
+
+import abstractfactory.factory.Factory;
+import abstractfactory.factory.Link;
+import abstractfactory.factory.Page;
+import abstractfactory.factory.Tray;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(0);
+        }
+        Factory factory = Factory.getFactory(args[0]);
+
+        Link asahi = factory.createLink("朝日新聞", "http://www.asahi.com/");
+        Link yomiuri = factory.createLink("読売新聞", "http://www.yomiuri.co.jp/");
+        Link us_yahoo = factory.createLink("Yahoo!", "http://www.yahoo.com");
+        Link jp_yahoo = factory.createLink("Yahoo!Japan", "http://www.yahoo.co.jp");
+        Link excite = factory.createLink("Excite", "http:/www.excite.com/");
+        Link google = factory.createLink("Google", "http://google.com/");
+
+        Tray traynews = factory.createTray("新聞");
+        traynews.add(asahi);
+        traynews.add(yomiuri);
+
+        Tray trayyahoo = factory.createTray("Yahoo!");
+        trayyahoo.add(us_yahoo);
+        trayyahoo.add(jp_yahoo);
+
+        Tray traysearch = factory.createTray("サーチエンジン");
+        traysearch.add(trayyahoo);
+        traysearch.add(excite);
+        traysearch.add(google);
+
+        Page page = factory.createPage("LinkPage", "motoyoshi");
+        page.add(traynews);
+        page.add(traysearch);
+        page.output();
+    }
+}
---
+++ src/abstractfactory/listfactory/ListFactory.java
@@ -0,0 +1,20 @@
+package abstractfactory.listfactory;
+
+import abstractfactory.factory.Factory;
+import abstractfactory.factory.Link;
+import abstractfactory.factory.Page;
+import abstractfactory.factory.Tray;
+
+public class ListFactory extends Factory {
+    public Link createLink(String caption, String url) {
+        return new ListLink(caption, url);
+    }
+
+    public Tray createTray(String caption) {
+        return new ListTray(caption);
+    }
+
+    public Page createPage(String title, String author) {
+        return new ListPage(title, author);
+    }
+}
